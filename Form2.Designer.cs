0be8de3824c363f0b911d9bac330e66f3677b044
oshricohen <oshrico24@gmail.com>
2017-02-13 18:53:45 +0200

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DSproject
+{
+    partial class FormSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(57, 244);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 31);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(13, 27);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(259, 20);
+            this.tbPath.TabIndex = 1;
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(93, 72);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(179, 20);
+            this.tbUser.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(93, 118);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(179, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(93, 174);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(179, 20);
+            this.tbName.TabIndex = 4;
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUser.Location = new System.Drawing.Point(2, 72);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(85, 22);
+            this.lbUser.TabIndex = 5;
+            this.lbUser.Text = "User Name";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(8, 118);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(75, 22);
+            this.lbPassword.TabIndex = 6;
+            this.lbPassword.Text = "Password";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.Location = new System.Drawing.Point(12, 172);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(48, 22);
+            this.lbName.TabIndex = 7;
+            this.lbName.Text = "Name";
+            // 
+            // FormSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 299);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnSave);
+            this.Name = "FormSetting";
+            this.Text = "setting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label lbName;
+    }
+}
