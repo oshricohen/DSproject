3733c9bfe023ba3401d2c82b4c4e65ef196f3e42
oshricohen <oshrico24@gmail.com>
2017-02-15 01:58:35 +0200

    שינוים
    הוספת קובץ קונפגורציה לתוכנית 
    שינוי מקום הפורט כעת הוא בחלונית הגדרות

--- DSproject/Form2.Designer.cs
+++ DSproject/Form2.Designer.cs
@@ -36,6 +36,9 @@
             this.lbUser = new System.Windows.Forms.Label();
             this.lbPassword = new System.Windows.Forms.Label();
             this.lbName = new System.Windows.Forms.Label();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.lbURL = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSave
@@ -46,12 +49,13 @@
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // tbPath
             // 
-            this.tbPath.Location = new System.Drawing.Point(13, 27);
+            this.tbPath.Location = new System.Drawing.Point(93, 27);
             this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(259, 20);
+            this.tbPath.Size = new System.Drawing.Size(179, 20);
             this.tbPath.TabIndex = 1;
             // 
             // tbUser
@@ -105,11 +109,41 @@
             this.lbName.TabIndex = 7;
             this.lbName.Text = "Name";
             // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPath.Location = new System.Drawing.Point(16, 214);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(40, 22);
+            this.lbPath.TabIndex = 8;
+            this.lbPath.Text = "Port";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(93, 214);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(179, 20);
+            this.tbPort.TabIndex = 9;
+            // 
+            // lbURL
+            // 
+            this.lbURL.AutoSize = true;
+            this.lbURL.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbURL.Location = new System.Drawing.Point(16, 27);
+            this.lbURL.Name = "lbURL";
+            this.lbURL.Size = new System.Drawing.Size(38, 22);
+            this.lbURL.TabIndex = 10;
+            this.lbURL.Text = "URL";
+            // 
             // FormSetting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 299);
+            this.Controls.Add(this.lbURL);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.lbPath);
             this.Controls.Add(this.lbName);
             this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.lbUser);
@@ -135,5 +169,8 @@
         private System.Windows.Forms.Label lbUser;
         private System.Windows.Forms.Label lbPassword;
         private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label lbURL;
     }
 }
--- DSproject/Form2.cs
+++ DSproject/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DSproject
 {
@@ -15,6 +16,20 @@ namespace DSproject
         public FormSetting()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("config.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(tbPath.Text);
+            sw.WriteLine(tbUser.Text);
+            sw.WriteLine(tbPassword.Text);
+            sw.WriteLine(tbName.Text);
+            sw.WriteLine(tbPort.Text);
+            sw.Close();
+            fs.Close();
+            this.Close();
         }
     }
 }
