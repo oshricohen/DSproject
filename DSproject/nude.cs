95860733b27338b64b13b433e2810a9f2bf6d9e5
oshricohen <oshrico24@gmail.com>
2017-02-16 05:56:25 +0200

    הוספת מחלקות
    הוספת מחלקת לרובוט ולצמתים

---
+++ DSproject/nude.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSproject
+{
+    class nude
+    {
+        static void Init()
+        {
+            //init nude
+        }
+        static void updateMe()
+        {
+            //update rubo about nude's info
+        }
+        static void sendRequistToRubo()
+        {
+            //ask Q 
+        }
+        static void sendMessegToChat()
+        {
+            //send messege to all nude 
+        }
+        static void updaeIPList()
+        {
+            // update all nude in list
+        }
+
+    }
+}
---
+++ DSproject/rubo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DSproject
+{
+    class rubo
+    {
+      static void Init()
+        {
+            //init rubo mood
+        }
+      static void getRequist()
+        {
+            //get requist from node
+        }
+      static void sendNudeList()
+        {
+            // list of nude
+        }
+      static void sendMesseg()
+        {
+            /*send messege to nude
+             * connect ACK
+             * Answer about requist*/
+        }
+
+
+    }
+}
