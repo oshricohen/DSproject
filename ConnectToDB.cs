8ed6b2d69ffa58e074acc4ecd66b08aa64a82e96
oshricohen <oshrico24@gmail.com>
2017-02-27 12:35:16 +0200

    Add files via upload

---
+++ ConnectToDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DSproject
+{
+    class ConnectToDB
+    {
+        private string path, userName, Password;
+        public ConnectToDB(string path, string userName, string password)
+        {
+            this.userName = userName;
+            this.path = path;
+            this.Password = password;
+
+            //connect to DB
+        }
+
+        public void uplodeFileToDB(File file)
+        {
+            //uplode file to DB
+        }
+
+        public File downloadsFileFromDB(string fileName)
+        {
+            //download file from DB
+        }
+
+        public string searchFile(string fileName)
+        {
+            // find file by name
+        }
+    }
+}
--- DSproject/nude.cs
+++ DSproject/nude.cs
@@ -3,30 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.ComponentModel;
 
 namespace DSproject
 {
     class nude
     {
-        static void Init()
+        private string allian;
+        private IPAddress myIP;
+        private Tuple<string, IPAddress> nudeName;  //item1=allian iteme2=myIP;
+        private List<Tuple<string, IPAddress>> neighbours=
+            new List<Tuple<string, IPAddress>>(); //list all neighbours
+        int UPdatePort = 30000 , messagePort=50000;//get/set nudename
+        StreamReader sIn;
+        StreamWriter sOut;
+        TcpListener updatelisiner;
+        TcpListener messageLisiner;
+        public nude(string allian)
         {
-            //init nude
+            this.allian = allian;
+            IPAddress[] localIP = Dns.GetHostAddresses("");
+            myIP = localIP[0];
+            foreach(IPAddress add in localIP)
+            {
+                if (!IPAddress.IsLoopback(add)&&add.AddressFamily==AddressFamily.InterNetwork)
+                {
+                    myIP = add;
+                    break;
+                }
+                updatelisiner = new TcpListener(myIP, UPdatePort);
+                updatelisiner.Start();
+                BackgroundWorker bwUpdate = new BackgroundWorker();
+                bwUpdate.WorkerSupportsCancellation = true;
+                bwUpdate.DoWork += new DoWorkEventHandler(this.background_update);
+
+                messageLisiner = new TcpListener(myIP, messagePort);
+                messageLisiner.Start();
+                BackgroundWorker bwChat = new BackgroundWorker();
+                bwChat.WorkerSupportsCancellation = true;
+                bwChat.DoWork += new DoWorkEventHandler(this.background_message);
+
+                
+                
+            }
+            nudeName = new Tuple<string, IPAddress>(allian, myIP);
         }
-        static void updateMe()
+        public void updateMe(IPAddress rubuIPAddress)
         {
-            //update rubo about nude's info
+            TcpClient me = new TcpClient(rubuIPAddress.ToString(), UPdatePort);
+            me.Connect(rubuIPAddress.ToString(), UPdatePort);
+            sOut = new StreamWriter(me.GetStream());
+            sOut.WriteLine(nudeName.Item1);
+            sOut.WriteLine(nudeName.Item2.ToString());
+            sOut.Flush();
+            sOut.Close();
+            me.Close();
+
         }
-        static void sendRequistToRubo()
+        public void sendRequistToRubo()
         {
             //ask Q 
         }
-        static void sendMessegToChat()
+        public void sendMessegToChat(string s)
         {
-            //send messege to all nude 
+             
         }
-        static void updaeIPList()
+        public void getMessage()
         {
-            // update all nude in list
+
+        }
+        public void updateIPList()
+        {
+            string name, sipAddres;
+            IPAddress ipaddres;
+            TcpClient getUpdate = updatelisiner.AcceptTcpClient();
+            sIn = new StreamReader(getUpdate.GetStream());
+            sIn.ReadLine();
+            name = sIn.ToString();
+            sIn.ReadLine();
+            sipAddres = sIn.ToString();
+            sIn.Close();
+            ipaddres = IPAddress.Parse(sipAddres);
+            Tuple  <string,IPAddress> tempt = new Tuple<string, IPAddress>(name, ipaddres);
+            neighbours.Add(tempt);
+                    
+        }
+        private void background_update (object sender ,DoWorkEventArgs e)
+        {
+            updateIPList();
+        }
+        private void background_message (object sender, DoWorkEventArgs e)
+        {
+            getMessage();
         }
 
     }
